d23d3643f8cb457544a4659cbb22ea869630ae63
Isabelle Padua <isaalvespadua@gmail.com>
2022-09-07 11:27:30 -0300

    Project created

---
+++ backend/minhaRevenda/src/main/java/com/isaAlves/minhaRevenda/MinhaRevendaApplication.java
@@ -0,0 +1,13 @@
+package com.isaAlves.minhaRevenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaRevendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaRevendaApplication.class, args);
+	}
+
+}
---
+++ backend/minhaRevenda/src/test/java/com/isaAlves/minhaRevenda/MinhaRevendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaAlves.minhaRevenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaRevendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
